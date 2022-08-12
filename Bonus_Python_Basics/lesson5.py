4f7f0852dac502f2873efdca8a179c8cc2bf1d68
Anton <Hiterik.main@gmail.com>
2022-08-12 07:18:42 +0300

    123

---
+++ Bonus_Python_Basics/lesson5.py
@@ -0,0 +1,7 @@
+print(2+3) # summ 5
+print(10/2) # delenie vsegda drob. tip float
+print(6%2) # ostatok ot delenia
+print(13//5) # delenie na celo
+print(5**2) # stepen
+print("2"+"3") # summ strok
+print(0.1+0.2)
---
+++ Bonus_Python_Basics/lesson6.py
@@ -0,0 +1,5 @@
+x=1
+my_var1=123
+_myvar1=123
+# 1_myvar1=123 nelzya
+print(type(x))
